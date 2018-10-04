36c985702628b0a837144c2d79d035c3e1d72338
EdvinAhlgren <ahlgrenedvin@gmail.com>
2018-10-04 18:02:41 +0200

    Add project files.

---
+++ LoveHeart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoveHeart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
